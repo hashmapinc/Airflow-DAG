d3784ccaa90587007cfe2a4603520b03579ccda3
John Aven (snoodleboot) <john.aven@hashmapinc.com>
2019-07-08 10:50:53 -0500

    Added Apache2.0 License Headers

--- dags/bash_test_triggers.py
+++ dags/bash_test_triggers.py
@@ -1,3 +1,18 @@
+#
+# Modifications © 2019 Hashmap, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
@@ -40,19 +55,19 @@ dag_layer_3.start_date = default_args['start_date']
 path = '/Users/johnaven/Sandbox/bash_dag_example'
 
 # STDOUT 'Hello World' with redirect to out.txt
-create_file= BashOperator(
+create_file = BashOperator(
     task_id='save-bash',
     bash_command='echo "Hello John" > {path}/out_tr.txt'.format(path=path)
 )
 
 # print the contents of out.txt to STDOUT
-print_file=BashOperator(
+print_file = BashOperator(
     task_id='print-file',
     bash_command='cat {path}/out_tr.txt'.format(path=path)
 )
 
 # clone/copy the data into another file
-copy_file=BashOperator(
+copy_file = BashOperator(
     task_id='copy-file',
     bash_command='cp {path}/out_tr.txt {path}/out_tr_copy.txt'.format(path=path)
 )
